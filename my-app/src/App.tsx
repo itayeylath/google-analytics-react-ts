5308c4dd09c55e0974c07ce91280ce7c586584bb
itayeylath <itay.ey@gmail.com>
2024-05-22 12:11:55 +0300

    add GoogleAnalyticsProvider to App.tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,29 +1,22 @@
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import './App.css';
-import { useEffect } from 'react';
-import ga from './providers/ga-init';
+import GoogleAnalyticsProvider from './providers/GoogleAnalyticsProvider';
 
 const App: React.FC = () => {
-  const location = useLocation();
-// TODO: add useEffect with this logic to your App.tsx 
-// If you prefer it more esthetic, you can implement it it as a component that return null (GARouteChangeTracker.tsx)
-  useEffect(() => {
-    ga.trackPageView(document.title, location.pathname + location.search);
-  }, [location]);
 
   return (
-    <Routes>
-      {/* TODO: uncomment or remove GARouteChangeTracker in  your use case */}
-      {/* <GARouteChangeTracker/> */}
-      <Route 
-        path="/" 
-        element={
-          <div>
-            Google Analytics React TypeScript
-          </div>
-        } 
-      />      
-    </Routes>
+    <GoogleAnalyticsProvider>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              Google Analytics React TypeScript
+            </div>
+          }
+        />
+      </Routes>
+    </GoogleAnalyticsProvider >
   );
 }
 
